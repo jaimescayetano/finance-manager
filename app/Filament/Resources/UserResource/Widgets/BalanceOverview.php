0fd1d1ed51de193dac2755ebdd8278c694816541
Jaimes Cayetano <jhonalexisjaimescayetano1@gmail.com>
2024-10-30 20:12:21 -0500

    feat: Add balance data

--- app/Filament/Resources/UserResource/Widgets/BalanceOverview.php
+++ app/Filament/Resources/UserResource/Widgets/BalanceOverview.php
@@ -2,6 +2,8 @@
 
 namespace App\Filament\Resources\UserResource\Widgets;
 
+use App\Models\Expense;
+use App\Models\Income;
 use App\Models\User;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
@@ -16,11 +18,11 @@ class BalanceOverview extends BaseWidget
                 ->descriptionIcon('heroicon-o-currency-dollar')
                 //->chart([7, 2, 10, 3, 15, 4, 17])
                 ->color('success'),
-            Stat::make('Expenses', '21%')
+            Stat::make('Expenses', Expense::getPercentaje(Expense::TYPE_ACTION) . '%')
                 ->description('7% increase')
                 ->descriptionIcon('heroicon-m-arrow-trending-down')
                 ->color('danger'),
-            Stat::make('Incomes', '25%')
+            Stat::make('Incomes', Income::getPercentaje(Income::TYPE_ACTION) . '%')
                 ->description('3% increase')
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
                 ->color('success'),
